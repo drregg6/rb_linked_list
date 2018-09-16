bbdb22ff7ab926c608a197eae94c8c48cf008bd7
Dave <drregg6@gmail.com>
2018-09-16 12:30:23 -0400

    completed .append and .prepend

--- index.rb
+++ index.rb
@@ -11,15 +11,18 @@ my_list.append("be")
 my_list.append("cheese")
 my_list.append("and")
 my_list.append("crackers")
+my_list.prepend("Dave:")
+my_list.prepend("Dear ")
 
-p my_list.node
-puts "The size of the linked list: #{my_list.size}"
-puts "\n"
-puts my_list.to_s
-puts "At index (1): #{my_list.at(1)}"
-puts "At index (4): #{my_list.at(4)}"
-puts "\n"
-puts "Head: #{my_list.head}"
-puts "Tail: #{my_list.tail}"
-puts "\n"
-p my_list.pop
+p my_list
+# p my_list.node
+# puts "The size of the linked list: #{my_list.size}"
+# puts "\n"
+# puts my_list.to_s
+# puts "At index (1): #{my_list.at(1)}"
+# puts "At index (4): #{my_list.at(4)}"
+# puts "\n"
+# puts "Head: #{my_list.head}"
+# puts "Tail: #{my_list.tail}"
+# puts "\n"
+# p my_list.pop
--- list.rb
+++ list.rb
@@ -7,7 +7,7 @@
 # head: returns the first node            #xx
 # tail: returns the last node             #xx
 # at(i): returns the node at index        #xx
-# pop: removes the last element           #
+# pop: removes the last element           #xx
 # contains?(el): TRUE if (el) exists      #
 # find(el): if TRUE returns index with el #
 # to_s: print entire list as string       #xx
@@ -24,12 +24,25 @@ class List
     end
 
 
-# appended nodes are added to the end of the list
-# the pointer should always point to nil
+# append pointers should always be pointing to nil
+# the previous pointer should then point to the new node
     def append(data)
-        @node_index = @list.size
         @list.push(Node.new(data))
-        @list[@node_index].node[:pointer] = @node_index + 1
+
+        if @list.size > 1
+            @list[-2].node[:pointer] = @list.size - 1
+        end
+
+        @list
+    end
+
+
+    def prepend(data)
+        @list.unshift(Node.new(data, 1))
+
+        @list.drop(1).each do |node|
+            node.node[:pointer] = (node.node[:pointer] + 1) if node.node[:pointer] != nil
+        end
 
         @list
     end
